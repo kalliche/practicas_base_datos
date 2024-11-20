ff9a3811aabc7db7e2ab330d1245fa87b69d67e3
unknown <kalliche16@gmail.com>
2024-11-20 17:27:29 -0500

    tabla completa de productos

--- Script_tienda.sql
+++ Script_tienda.sql
@@ -4,3 +4,11 @@ CREATE TABLE productos(
 	marca_id INTEGER,
 	precio REAL
 );
+
+INSERT INTO productos (nombre, marca_id, precio)
+	VALUES ('iPhone 14', 1, 999.99);
+
+SELECT * FROM productos p;
+
+INSERT INTO productos (nombre, marca_id, precio)
+	VALUES ('Smartplone', 2, 800.00),('Table', 1, 400.00);
